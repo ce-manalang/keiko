3276381bafc94f151b368e7c3386baf9ce1b802b
ce <cecillemanalang@gmail.com>
2026-02-07 17:43:02 +0800

    feat: user crud scheduler only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :authenticate_user!
+
+  private
+
+  def require_scheduler!
+    return if current_user&.scheduler?
+
+    redirect_to root_path, alert: "Not authorized."
+  end
+
+  def require_employee!
+    return if current_user&.employee?
+
+    redirect_to root_path, alert: "Not authorized."
+  end
 end
--- db/migrate/20260207074930_create_users.rb
+++ db/migrate/20260207074930_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :email,              null: false, default: ""
       t.string :employee_id
       t.integer :role
 
