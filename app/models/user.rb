133c7eba5db6f1b492b9c39107ad4486a64960d2
ce <cecillemanalang@gmail.com>
2026-02-07 16:21:39 +0800

    feat: add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  enum role: { employee: 0, scheduler: 1 }
+  enum :role, { employee: 0, scheduler: 1 }
+
+  has_many :shifts, dependent: :destroy
 
   validates :name, :email, :employee_id, :role, presence: true
   validates :email, :employee_id, uniqueness: true
