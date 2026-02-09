d2a8200099abebe158bc4c965cf4891d99afca46
ce <cecillemanalang@gmail.com>
2026-02-10 05:49:59 +0800

    feat: shift crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users do
+    resources :shifts, except: [ :show ]
+  end
 
   get "home/index"
   root "home#index"
